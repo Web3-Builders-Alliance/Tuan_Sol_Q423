0f2658aab2fb23d452addbe2020291f8066d8514
Tuan Pham Minh <tuanpmhd@gmail.com>
2023-11-27 23:28:26 +0700

    add spl_transfer

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -10,18 +10,22 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("9xXa31Mnqxsnhd27uz4UTyrztzHA2f15bqb2dJ3puPhD");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("7ZsXMgLFxb2diaU92zsrfvpoCeUBhiSZPG6tZ73LV7Zn");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        let fromWalletATA = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        let toWalletATA = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to)
 
         // Transfer the new token to the "toTokenAccount" we just created
+        let txId = await transfer(connection, keypair, fromWalletATA.address, toWalletATA.address, keypair, 1)
+        console.log("Your transfer txId:", txId)
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
